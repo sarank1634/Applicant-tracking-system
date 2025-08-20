a7178a03f402bb4f4859f76bfbb3db4c97f394ef
sarank1634 <saravanakumar1634@gmail.com>
2025-08-20 21:36:37 +0530

    34.23 hasbeen copleted

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,17 +1,24 @@
 import { Link } from 'react-router';
-
-const ResumeCard = ({resume}: {resume: {id, companyName, jobTitle}}) => {
+import ScoreCircle from './ScoreCircle';
+const ResumeCard = ({resume}: {resume: {id, companyName, jobTitle, feedback}}) => {
   return (
     <Link to={`/resume/${resume.id}`} className="resume-card animate-in fade-in duration-1000">
-        <div className="flex flex-col gap-2 ">
+      <div className='resume-card-header flex'>
+      <div className="flex flex-col gap-2 ">
             <h2 className='!text-black font-bold  break-words'> {resume.companyName} </h2>
             <h3 className='!text-lg break-words text-gray-500'>{resume.jobTitle}</h3>
         </div>
           <div className='flex-shirnk-0'>
-
-  <img src={resume.imagePath} alt="" className='w-full border rounded-lg shadow-sm' />           
-  
+             <ScoreCircle score={resume.feedback.overallScore} />
+             </div>
+      </div>
+        <div className="gradient-border anime-in fade duration-1000">
+          <div className="w-full h-full">
+            <img src={resume.imagePath} alt="" className='w-full border rounded-lg shadow-sm' />           
           </div>
+        </div>
+  
+         
     </Link>
   );
 };
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     <Navbar />
 
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track your Application & Resume Rating</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
