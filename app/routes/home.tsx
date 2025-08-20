5f8832502f38bf882290838f9905dd3721845ea3
sarank1634 <saravanakumar1634@gmail.com>
2025-08-20 21:46:41 +0530

    34.50

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return(
    <main className="bg-[url('/images/bg-main.svg')] bg-cover " >
     <Navbar />
-
+    
     <section className="main-section">
       <div className="page-heading py-16">
         <h1>Track your Application & Resume Rating</h1>
