22ce6fd09daba239d94c0489ff0f5642b2a4d5a4
sarank1634 <saravanakumar1634@gmail.com>
2025-08-20 17:38:35 +0530

    main view hasbeen setteled successfully

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: " RSmart feedback for your dream jb!uter!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return(
+   <main className="bg-[url('/images/bg-main.svg')] bg-cover " >
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track your Application & Resume Rating</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
+  </main>
+  );
 }
