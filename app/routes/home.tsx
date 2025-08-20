18d9a6f6c96e1a6e71f7cf331e4bb90878ab5bde
sarank1634 <saravanakumar1634@gmail.com>
2025-08-20 18:18:24 +0530

    navbar index.ts  and index.d.ts has been added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
-
+import Navbar from "../components/Navbar";
+import { resumes } from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,12 +12,21 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return(
    <main className="bg-[url('/images/bg-main.svg')] bg-cover " >
+    <Navbar />
+
     <section className="main-section">
       <div className="page-heading">
         <h1>Track your Application & Resume Rating</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
     </section>
+
+
+    {resumes.map((resume) => (
+      <div>
+        <h1>{resume.jobTitle}</h1>
+      </div>
+    ))}
   </main>
   );
 }
