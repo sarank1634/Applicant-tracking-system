d2c9b1fe9d75800e58f1d08c0a067766fc1e4d3b
sarank1634 <saravanakumar1634@gmail.com>
2025-08-22 13:51:28 +0530

    set up puter sotre inside auth.tsx

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
-  // route("/resume/:id", "routes/resume.tsx"),
+  route("/resume/:id", "routes/resume.tsx"),
   route("/auth", "routes/auth.tsx"),
   
 ] satisfies RouteConfig;
--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
-
+import { Welcome } from '~/welcome/welcome'
+import { usePuterStore } from '../lib/puter'
 export const meta = () => ([
     {title: 'Resumind | Auth'},
     {name: 'description', content: 'log into your account'},
 ])
 const auth = () => {
+  const {isLoading} = usePuterStore();
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center ">
-        <div className="gradient-border shadow-lg"></div>
+        <div className="gradient-border shadow-lg">
+          <section className='flex flex-col gap-8 bg-white rounded-2xl p-10'>
+             <div className="flex flex-col items-center gap-2 text-center" >
+            <h1>Welcome</h1>
+            <h2>Log in to your account</h2>
+             </div>
+          </section>
+        </div>
     </main>
   )
 }
