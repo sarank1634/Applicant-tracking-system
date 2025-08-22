99573a89d79f132fe44fbb61fc4b8f6a66c7ca27
sarank1634 <saravanakumar1634@gmail.com>
2025-08-22 18:11:57 +0530

    adding form inside upload component

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, type FormEvent } from 'react'
 import Navbar from '~/components/Navbar'
 
 
@@ -6,13 +6,17 @@ const upload = () => {
    const[isPorcessing, setProcessing] = useState(true)
    const[statusText, setStatusText] = useState('')
 
+
+   const handleSubmit = (e: FormEvent<HTMLFormElement >) => {
+
+   }
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover " >
     <Navbar />
 
 
     <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
             <h1 >Smart feedback for your dream job</h1>
             {isPorcessing ? (
                <>
@@ -21,6 +25,20 @@ const upload = () => {
                </>
             ):(
                 <h2>Drop your resume for an ATS score and improvement tips</h2>
+            )}
+            {!is Processing && (
+
+                <form id="upload -form" onSubmit={handleSubmit} className='flex flex-col gap-4 mt-8'>
+                    <div className="form-div">
+                        <label htmlFor="company-name">Company Name</label>
+                        <input type="text" name='company-name' placeholder='Company Name' id='company-name' />
+                    </div>
+                    <div className="form-div">
+                        <label htmlFor="job-title">Company Name</label>
+                        <input type="text" name='job-title' placeholder='Job Title' id='job-title' />
+                    </div>
+                </form>
+
             )}
         </div>
     </section>
