63d07c77130a9f52e589adb1f87a9453fbca8e1e
sarank1634 <saravanakumar1634@gmail.com>
2025-08-22 19:06:57 +0530

    uploaded has been created

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import type { File } from 'buffer';
+import {useState, useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
 
-const FileUploader = () => {
+interface FileUploader {
+  onFileSelect? : (file: File | null) => void;
+}
+const FileUploader = ({onFileSelect} : FileUploaderProps) => { 
+   const [file, setFile] = useState();
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        const file: File = acceptedFiles[0] || null; 
+    }, [])
+
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  
   return (
-    <div className='w-full gradient-border'>FileUploader</div>
+    <div className='w-full gradient-border'>
+       <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      <div className="space-y-4 cursor-pointer">
+        <div className='mx-auto w-16 h-16 flex items-center justify-center'>
+           <img src="/icons/info.svg" alt="upload" className='size-20' />
+        </div>
+
+        {file ?  (
+             <div>
+
+
+
+         </div>
+
+
+        ) : (
+          <div>
+            <p className='text-lg text-gray-500 '>
+              <span className='font-semibold'>
+                Click to Upload
+              </span>
+            </p>
+            <p className='text-lg text-gray-500'>PDF(max 20 MB) </p>
+          </div>
+        )}
+      </div>
+    </div>
+    </div>
   )
 }
 
-export default FileUploader
+export default FileUploader;
