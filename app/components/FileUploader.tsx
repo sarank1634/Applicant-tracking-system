4f4d1c2104742232a07263eb55f0e05a6412fae5
sarank1634 <saravanakumar1634@gmail.com>
2025-08-22 21:00:58 +0530

    file uploaded successfully

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -51,7 +51,9 @@ const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
                   </p>
                 </div>
               </div>
-              <button>
+              <button className='p-2 cursor-pointer' onClick={(e) => {
+                onFileSelect?.(null)
+              }}>
                 <img src="/icons/cross.svg" alt="remove" className='w-4 h-4' />
               </button>
             </div>
