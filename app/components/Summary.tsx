ca3692032be4f82ca034e20f7894a047b759802d
sarank1634 <saravanakumar1634@gmail.com>
2025-08-23 17:01:57 +0530

    adding custom category

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ScoreGauge from './ScoreGauge'
 
+const Category = ({})
+
 const Summary = ({ feedback }: { feedback: Feedback }) => {
   return (
     <div className='bg-white rounded-2xl shadow-md w-full'>
       <div className="flex flex-row items-center gap-8 p-4">
         <ScoreGauge score={ feedback.overallScore }/>
+
+        <div className='flex flex-col gap-2'>
+          <h2 className='text-2xl font-bold'>Your Resume Score</h2>
+          <p className='text-gray-500'>This score is calculated based on the variable listed below</p>
+        </div>
+
+
+
       </div>
     </div>
   )
