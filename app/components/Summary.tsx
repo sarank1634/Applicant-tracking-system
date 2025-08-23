3568a2133a05161cdbe5af6b8ac68420584838bd
sarank1634 <saravanakumar1634@gmail.com>
2025-08-23 16:55:51 +0530

    feed back props added

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import ScoreGauge from './ScoreGauge'
 
-const Summary = () => {
+const Summary = ({ feedback }: { feedback: Feedback }) => {
   return (
     <div className='bg-white rounded-2xl shadow-md w-full'>
       <div className="flex flex-row items-center gap-8 p-4">
-        
+        <ScoreGauge score={ feedback.overallScore }/>
       </div>
     </div>
   )
